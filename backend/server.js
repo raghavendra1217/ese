79f2ed3979ebc2c571e24efe87c9ec2b2a70783e
raghavendra1217 <raghavendrak.bsc23@rvu.edu.in>
2025-07-26 22:47:03 +0530

    manifest.json

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images/products',express.static(path.join(__dirname, 'public/images/products')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build/manifest.json'));
 });
 
 // --- Server Start-up Logic ---
